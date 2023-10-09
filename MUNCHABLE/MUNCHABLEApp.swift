06477fc7ae385f4591714b358aeab99ac403b16a
non1414 <nouf22228@gmail.com>
2023-10-09 12:12:42 +0300

    Initial Commit

---
+++ MUNCHABLE/MUNCHABLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUNCHABLEApp.swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MUNCHABLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
