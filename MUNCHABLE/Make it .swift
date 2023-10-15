76a9f290313205b2b28c29601b6c0abcb379548f
non1414 <nouf22228@gmail.com>
2023-10-15 13:52:47 +0300

    first commit

--- MUNCHABLE/Make it .swift
+++ MUNCHABLE/Make it .swift
@@ -6,10 +6,85 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
+
+
+struct PlayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return LoopingPlayerUIView(frame: .zero)
+    }
+}
+
+
+class LoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        // Load the resource
+        let fileUrl = Bundle.main.url(forResource: "demoVideo", withExtension: "mp4")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+
+        // Setup the player
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+
+        // Start the movie
+        player.play()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
+
 
 struct Make_it_: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        GeometryReader{ geo in
+            ZStack {
+                PlayerView()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width, height: geo.size.height+100)
+                    .overlay(Color.black.opacity(0.2))
+                    .blur(radius: 1)
+                .edgesIgnoringSafeArea(.all)
+
+                VStack{
+                    Text("Welcome to our world of culinary adventures! ")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    Spacer()
+                    Text("Get ready to explore mouthwatering recipes and elevate your cooking skills with Munchable ! ")
+                        .font(.caption)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                }
+            }
+        }
+
     }
 }
 
--- MUNCHABLE/Onboarding.swift
+++ MUNCHABLE/Onboarding.swift
@@ -8,11 +8,80 @@
 import SwiftUI
 
 struct Onboarding: View {
+    @State private var currentPage = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            TabView(selection: $currentPage) {
+                ForEach(0..<onboardingData.count) { index in
+                    OnboardingPageView(onboardingItem: onboardingData[index])
+                        .tag(index)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .navigationBarHidden(true)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    if currentPage > 0 {
+                        Button("Previous") {
+                            currentPage -= 1
+                        }
+                        .padding()
+                    }
+                    
+                    Spacer()
+                    
+                    if currentPage == onboardingData.count - 1 {
+                        Button("Get Started") {
+                            // Handle the action when the user wants to start using the app
+                        }
+                        .padding()
+                    } else {
+                        Button("Next") {
+                            currentPage += 1
+                        }
+                        .padding()
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    Onboarding()
+struct OnboardingPageView: View {
+    let onboardingItem: OnboardingItem
+    
+    var body: some View {
+        VStack {
+            Image(onboardingItem.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            Text(onboardingItem.title)
+                .font(.title)
+                .padding()
+            
+            Text(onboardingItem.description)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
+    }
 }
+
+struct OnboardingItem {
+    let imageName: String
+    let title: String
+    let description: String
+}
+
+let onboardingData: [OnboardingItem] = [
+    OnboardingItem(imageName: "onboarding_image_1", title: "Latest Technologies", description: "Use Make it to explore an inspiring recipes to add a little fun to your dinner table by using AI "),
+    OnboardingItem(imageName: "onboarding_image_2", title: "Get Inspired", description: "Don’t know what to eat? we’ll suggest things to cook with the ingredients you have"),
+  
+]
+
