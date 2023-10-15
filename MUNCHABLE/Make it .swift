a3160c3c107ab0fd3fab0f19247f982ebc7749f8
non1414 <nouf22228@gmail.com>
2023-10-15 13:42:27 +0300

    first commit

---
+++ MUNCHABLE/Make it .swift
@@ -0,0 +1,18 @@
+//
+//  Make it .swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 29/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Make_it_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Make_it_()
+}
