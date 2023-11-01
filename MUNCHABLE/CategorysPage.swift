19f2a896eeca079905a235b494626db47a3f3c45
non1414 <nouf22228@gmail.com>
2023-11-01 22:23:40 +0300

    categroys page

---
+++ MUNCHABLE/CategorysPage.swift
@@ -0,0 +1,18 @@
+//
+//  CategorysPage.swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct CategorysPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategorysPage()
+}
---
+++ MUNCHABLE/LoadingRecipesPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingRecipesPage.swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct LoadingRecipesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingRecipesPage()
+}
