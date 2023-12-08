46707717eb3291787f2825e9cb55901900a5dd62
non1414 <nouf22228@gmail.com>
2023-12-08 21:31:32 +0300

    Activate the filter in the search engine

---
+++ MUNCHABLE/RecipeFromAi.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeFromAi.swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 29/04/1445 AH.
+//
+
+import SwiftUI
+
+struct RecipeFromAi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeFromAi()
+}
---
+++ MUNCHABLE/ingredientsPaje.swift
@@ -0,0 +1,18 @@
+//
+//  ingredientsPaje.swift
+//  MUNCHABLE
+//
+//  Created by نوف بخيت الغامدي on 29/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ingredientsPaje: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ingredientsPaje()
+}
